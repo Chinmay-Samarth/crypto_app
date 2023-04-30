2ba93982bf876a84d1f37f234ab9bb4c83954fb0
Chinmay <chinmaydhondale@gmail.com>
2023-04-30 12:11:28 +0530

    30/4

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,18 +1,32 @@
 import React from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import axios from "axios";
 
 export const Home = () => {
-    const textstyle = {
-        position: "relative",
-        left: "40vh"
+  let data;
+  if(data===undefined){
+      axios.get("https://api.coincap.io/v2/assets").then((response) => {
+          console.log(response.request.response);
+          data = response.request.response;
+        });
     }
-  return (
+        return (
     <>
-    <div className="container text-center " style={{marginTop:"5vh"}}>
-        <input type="text" className="input-group w-50 "  style={textstyle} />
-    </div>
-    <div className="container my-3 border">
-
-    </div>
+      <div
+        className="container text-center d-flex "
+        style={{ marginTop: "5vh", justifyContent: "center" }}
+      >
+        <input type="text" className="input-group w-50  " />
+      </div>
+      <div className="container my-3 border">
+        {/* <DataTable >
+            <Column field="name" header="Currency Name" />
+            <Column field="symbol" header="Symbbol" />
+            $<Column field="priceUsd" header="Price (in $)" />
+            <Column field="supply" header="Supply" />
+        </DataTable> */}
+      </div>
     </>
   );
 };
